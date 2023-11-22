6c341a250b66d4f8a57333fe8298e8ed06866f92
Frank Denis <github@pureftpd.org>
2023-11-22 19:13:12 +0100

    Switch to an external package for BoundedArray
    https://github.com/ziglang/zig/pull/18067

--- build.zig
+++ build.zig
@@ -4,12 +4,15 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const bounded_array_module = b.dependency("bounded_array", .{}).module("bounded_array");
+
     const lib = b.addStaticLibrary(.{
         .name = "hpke",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
+    lib.addModule("bounded_array", bounded_array_module);
     b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
@@ -17,6 +20,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    main_tests.addModule("bounded_array", bounded_array_module);
 
     const main_tests_run = b.addRunArtifact(main_tests);
 
